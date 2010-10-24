51c3ee095fd8f28180b46cab69c3405f36841f15
czy88840616 <czy88840616@e8d69979-91ed-93f9-aef3-9e7470018f69>
2010-10-24 16:01:47 +0000

    加入refer的debug判断

---
+++ trunk/ucool/src/main/java/common/HttpTools.java
@@ -0,0 +1,27 @@
+package common;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author <a href="mailto:czy88840616@gmail.com">czy</a>
+ * @since 10-10-24 下午11:52
+ */
+public class HttpTools {
+
+    /**
+     * 判断refer中是否带debug=true
+     *
+     * @param request of type HttpServletRequest
+     * @return boolean
+     */
+    public static boolean isReferDebug(HttpServletRequest request) {
+        String refer = request.getHeader("Referer");
+        /**
+         * 目前支持2种方式：?debug和debug=true
+         */
+        if(refer != null && (refer.indexOf("?debug") != -1 || refer.indexOf("debug=true") != -1)) {
+            return true;
+        }
+        return false;
+    }
+}
