382afa83f4738489572bdf5cd32df5f241a8ed64
czy88840616 <czy88840616@e8d69979-91ed-93f9-aef3-9e7470018f69>
2010-10-27 11:03:01 +0000

    加入时间程序测试代码

---
+++ trunk/ucool/src/test/java/QuartzJob/QuartzJob.java
@@ -0,0 +1,11 @@
+package QuartzJob;
+
+/**
+ * @author <a href="mailto:zhangting@taobao.com">张挺</a>
+ * @since 10-10-27 下午3:41
+ */
+public class QuartzJob {
+    public void work() {
+        System.out.println("Quartz的任务调度！！！");
+    }
+}
---
+++ trunk/ucool/src/test/java/QuartzJob/QuartzJobTest.java
@@ -0,0 +1,15 @@
+package QuartzJob;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author <a href="mailto:zhangting@taobao.com">уем╕</a>
+ * @since 10-10-27 обнГ3:53
+ */
+public class QuartzJobTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        
+    }
+}
